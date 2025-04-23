6d21051fe66717b1fc6703af2e31153bdeeb9851
AnGe <ge.an2@northeastern.edu>
2025-04-22 20:24:57 -0700

    update cors

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,8 @@ app.use(
   cors({
     origin: "https://an-ge-project3-1.onrender.com",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
